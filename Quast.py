3b4e9a80b4fa68381d8cfe70376c3a80c107c043
Shubhangk <smkulka2@illinois.edu>
2021-06-15 11:52:49 -0500

    major refactoring of Quast.py

--- Quast.py
+++ Quast.py
@@ -44,27 +44,6 @@ class Quast:
         union_quast.root_node = union_quast.__union(node1=self.root_node, node2=quast.root_node)
         return union_quast
 
-    def __union(self, node1, node2):
-        if node1 is None:
-            if node2.node_type is node2.IN_NODE:
-                return self.in_node
-            elif node2.node_type is node2.OUT_NODE:
-                return self.out_node
-            else:
-                true_branch_node = self.__union(node1=node1, node2=node2.true_branch_node)
-                false_branch_node = self.__union(node1=node1, node2=node2.false_branch_node)
-                return Node(constraint=node2.constraint, false_branch_node=false_branch_node,
-                            true_branch_node=true_branch_node)
-        if node1.node_type is node1.IN_NODE:
-            return self.in_node
-        elif node1.node_type is node1.OUT_NODE:
-            return self.__union(node1=None, node2=node2)
-        else:
-            true_branch_node = self.__union(node1=node1.true_branch_node, node2=node2)
-            false_branch_node = self.__union(node1=node1.false_branch_node, node2=node2)
-            return Node(constraint=node1.constraint, false_branch_node=false_branch_node,
-                        true_branch_node=true_branch_node)
-
     def complement(self):
         complement_quast = Quast(space=self.get_space())
         complement_quast.root_node = Node(constraint=self.root_node.constraint,
@@ -74,22 +53,42 @@ class Quast:
                                                                                  complement_quast))
         return complement_quast
 
-    def __complement(self, curr_node, complement_quast):
-        if curr_node == self.in_node:
-            return complement_quast.out_node
-        elif curr_node == self.out_node:
-            return complement_quast.in_node
-        else:
-            return Node(constraint=curr_node.constraint,
-                        true_branch_node=self.__complement(curr_node.true_branch_node, complement_quast),
-                        false_branch_node=self.__complement(curr_node.false_branch_node, complement_quast))
-
     def intersect(self, quast):
         if self.get_space() != quast.get_space():
             raise Exception("spaces don't match")
         intersection_quast = Quast(space=self.get_space())
         intersection_quast.root_node = intersection_quast.__intersect(node1=self.root_node, node2=quast.root_node)
         return intersection_quast
+
+    def reconstruct_set(self):
+        basic_set_list = self.__reconstruct_set(self.root_node, [])
+        final_set = basic_set_list[0]
+        for basic_set in basic_set_list:
+            final_set = final_set.union(basic_set)
+        return final_set
+
+    # Description: proxy function for recursively printing QUAST level-by-level
+    def print_tree(self):
+        self.__print_tree(node=self.root_node, level=0)
+
+    def visualize_tree(self):
+        dot = Digraph(format="pdf", comment='Quast Visualization')
+        arcs = {}
+        self.__visualize_tree(arcs, self.root_node)
+        for arc in arcs.keys():
+            print(arc)
+            dot.edge(arc[0], arc[1], label=arcs[arc])
+        print(dot.source)
+        dot.render('visualization-output/quast', view=True)
+
+    def __negate_constraint(self, constraint):
+        coefficients = constraint.get_coefficients_by_name()
+        for var in coefficients.keys():
+            coefficients[var] = coefficients[var].neg()
+
+        negated_constraint = constraint.set_coefficients_by_name(coefficients)
+        negated_constraint = negated_constraint.set_constant_val(negated_constraint.get_constant_val() - 1)
+        return negated_constraint
 
     def __intersect(self, node1, node2):
         if node1 is None:
@@ -112,12 +111,15 @@ class Quast:
             return Node(constraint=node1.constraint, false_branch_node=false_branch_node,
                         true_branch_node=true_branch_node)
 
-    def reconstruct_set(self):
-        basic_set_list = self.__reconstruct_set(self.root_node, [])
-        final_set = basic_set_list[0]
-        for basic_set in basic_set_list:
-            final_set = final_set.union(basic_set)
-        return final_set
+    def __complement(self, curr_node, complement_quast):
+        if curr_node == self.in_node:
+            return complement_quast.out_node
+        elif curr_node == self.out_node:
+            return complement_quast.in_node
+        else:
+            return Node(constraint=curr_node.constraint,
+                        true_branch_node=self.__complement(curr_node.true_branch_node, complement_quast),
+                        false_branch_node=self.__complement(curr_node.false_branch_node, complement_quast))
 
     def __reconstruct_set(self, curr_node, curr_constraints):
         if curr_node is self.out_node:
@@ -134,23 +136,10 @@ class Quast:
             curr_constraints.append(curr_node.constraint)
             bsets_from_true = self.__reconstruct_set(curr_node.true_branch_node, curr_constraints)
             curr_constraints.pop()
-            curr_constraints.append(self.negate_constraint(curr_node.constraint))
+            curr_constraints.append(self.__negate_constraint(curr_node.constraint))
             bsets_from_false = self.__reconstruct_set(curr_node.false_branch_node, curr_constraints)
             curr_constraints.pop()
             return bsets_from_true + bsets_from_false
-
-    def negate_constraint(self, constraint):
-        coefficients = constraint.get_coefficients_by_name()
-        for var in coefficients.keys():
-            coefficients[var] = coefficients[var].neg()
-
-        negated_constraint = constraint.set_coefficients_by_name(coefficients)
-        negated_constraint = negated_constraint.set_constant_val(negated_constraint.get_constant_val() - 1)
-        return negated_constraint
-
-    # Description: proxy function for recursively printing QUAST level-by-level
-    def print_tree(self):
-        self.__print_tree(node=self.root_node, level=0)
 
     # Reference -- https://stackoverflow.com/questions/34012886/print-binary-tree-level-by-level-in-python
     # Description: Recursively prints QUAST level-by-level
@@ -160,15 +149,26 @@ class Quast:
             print(' ' * 5 * level + '--->', node.constraint)
             self.__print_tree(node.false_branch_node, level + 1)
 
-    def visualize_tree(self):
-        dot = Digraph(format="pdf", comment='Quast Visualization')
-        arcs = {}
-        self.__visualize_tree(arcs, self.root_node)
-        for arc in arcs.keys():
-            print(arc)
-            dot.edge(arc[0], arc[1], label=arcs[arc])
-        print(dot.source)
-        dot.render('visualization-output/quast', view=True)
+    def __union(self, node1, node2):
+        if node1 is None:
+            if node2.node_type is node2.IN_NODE:
+                return self.in_node
+            elif node2.node_type is node2.OUT_NODE:
+                return self.out_node
+            else:
+                true_branch_node = self.__union(node1=node1, node2=node2.true_branch_node)
+                false_branch_node = self.__union(node1=node1, node2=node2.false_branch_node)
+                return Node(constraint=node2.constraint, false_branch_node=false_branch_node,
+                            true_branch_node=true_branch_node)
+        if node1.node_type is node1.IN_NODE:
+            return self.in_node
+        elif node1.node_type is node1.OUT_NODE:
+            return self.__union(node1=None, node2=node2)
+        else:
+            true_branch_node = self.__union(node1=node1.true_branch_node, node2=node2)
+            false_branch_node = self.__union(node1=node1.false_branch_node, node2=node2)
+            return Node(constraint=node1.constraint, false_branch_node=false_branch_node,
+                        true_branch_node=true_branch_node)
 
     def __visualize_tree(self, arcs, node):
         if node.is_terminal():
