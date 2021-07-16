ed334a1bca04e534a367ace39d4a03531f669226
Shubhangk <smkulka2@illinois.edu>
2021-07-16 10:17:30 -0500

    removed overridden __setattr__ function -- cause of significant computational overhead

--- V2/Node.py
+++ V2/Node.py
@@ -2,25 +2,8 @@ import inspect
 
 
 class Node(object):
-    # Constant representing whether terminal node
     TERMINAL = 1
     NON_TERMINAL = 0
-    #
-    # # Information about (in)equality represented by node
-    # bset = None
-    # node_type = None
-    #
-    # # Pointers to true/false subtrees and parent node. Note that there may be multiple parents as the graph is a DAG
-    # true_branch_node = None
-    # false_branch_node = None
-
-    # Description: overriding this function should make the class immutable (except at initialization)
-    def __setattr__(self, *args):
-        # allow write only during initialization
-        if inspect.stack()[1][3] == '__init__':
-            object.__setattr__(self, *args)
-        else:
-            raise TypeError('Attempting to modify an immutable Node object')
 
     def __init__(self, bset, false_branch_node=None, true_branch_node=None, node_type=0):
         self.true_branch_node = true_branch_node
